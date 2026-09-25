8a028d8ae7789f9acc7212a3a207e3eb7760980b
zya-r <mezyasezen@gmail.com>
2026-09-25 13:04:52 +0530

    Delete features.py

--- features.py
+++
@@ -1,9 +0,0 @@
-from FlowCytometryTools import FCMeasurement
-
-f = "/storage/mezya.sezen/mphasis/dataset/final_scaled/BLAST110_100_P1_Cleaned_transformed_scaled.fcs"
-s = FCMeasurement(ID="x", datafile=f)
-
-for i in range(1, 40):
-    print(i,
-          "Detector:", s.meta.get(f"$P{i}N"),
-          "Marker:", s.meta.get(f"$P{i}S"))
